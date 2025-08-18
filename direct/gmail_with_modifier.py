e28c93799725dd031489cc41e111da149f6614e5
Ravi Madabhushi <ravi.madabhushi@scalekit.com>
2025-08-18 14:34:27 -0700

    added langgraph ways of using mcpserver and react agent

--- direct/gmail_with_modifier.py
+++ direct/gmail_with_modifier.py
@@ -6,20 +6,41 @@ from scalekit.connect.types import ToolInput, ToolOutput
 
 # Load environment variables
 load_dotenv()
-
+user_id = "user_1234567890"
 scalekit = scalekit.client.ScalekitClient(
     client_id=os.getenv("SCALEKIT_CLIENT_ID"),
     client_secret=os.getenv("SCALEKIT_CLIENT_SECRET"),
     env_url=os.getenv("SCALEKIT_ENV_URL"),
 )
 connect = scalekit.connect
-link_response = connect.get_authorization_link(
-    connection_name="GMAIL",
-    identifier="default",
-)
 
-print("click on the link to authorize gmail", link_response.link)
-input("Press Enter after authorizing gmail...")
+def authenticate_tool(connection_name, identifier):
+    
+    try:
+        response = connect.get_connected_account(
+            connection_name=connection_name,
+            identifier=identifier
+        )
+        if(response.connected_account.status != "ACTIVE"):
+            print(f"{connection_name} is not connected: {response.connected_account.status}")
+            link_response = connect.get_authorization_link(
+                connection_name=connection_name,
+                identifier=identifier
+            )
+            print(f"🔗click on the link to authorize {connection_name}", link_response.link)
+            input(f"⎆ Press Enter after authorizing {connection_name}...")
+    except Exception as e:
+        link_response = connect.get_authorization_link(
+            connection_name=connection_name,
+            identifier=identifier
+        )
+        print(f"🔗 click on the link to authorize {connection_name}", link_response.link)
+        input(f"⎆ Press Enter after authorizing {connection_name}...")
+    
+    return True
+
+authenticate_tool("GMAIL", user_id)
+
 
 @connect.post_modifier(tool_names=["gmail_fetch_mails"])
 def gmail_post_modifier(output:ToolOutput):
@@ -33,12 +54,12 @@ def gmail_pre_modifier(tool_input: ToolInput):
     return tool_input
 
 
-response = scalekit.connect.execute_tool(
+response = connect.execute_tool(
     tool_name="gmail_fetch_mails",
-    identifier="default",
+    identifier=user_id,
     tool_input={
         "max_results" : 1
     },
 )
 
-print(response)
+print(f"✅ Results: {response}")
--- mcp/main.py
+++ mcp/main.py
@@ -1,5 +1,6 @@
 
 import os
+import asyncio
 from dotenv import load_dotenv
 import scalekit.client
 from scalekit.connect.types import ToolMapping
@@ -11,14 +12,42 @@ scalekit_client = scalekit.client.ScalekitClient(
     os.getenv("SCALEKIT_CLIENT_ID"),
     os.getenv("SCALEKIT_CLIENT_SECRET")
 )
+connect = scalekit_client.connect
 
-def main():
+def authenticate_tool(connection_name, identifier):
+    
+    try:
+        response = connect.get_connected_account(
+            connection_name=connection_name,
+            identifier=identifier
+        )
+        print(response)
+        if(response.connected_account.status != "ACTIVE"):
+            print(f"{connection_name} is not connected: {response.connected_account.status}")
+            link_response = connect.get_authorization_link(
+                connection_name=connection_name,
+                identifier=identifier
+            )
+            print(f"click on the link to authorize {connection_name}", link_response.link)
+            input(f"Press Enter after authorizing {connection_name}...")
+    except Exception as e:
+        link_response = connect.get_authorization_link(
+            connection_name=connection_name,
+            identifier=identifier
+        )
+        print(f"click on the link to authorize {connection_name}", link_response.link)
+        input(f"Press Enter after authorizing {connection_name}...")
+    
+    return True
+
+async def main():
 
     #create connected account for identifier default and connection name GMAIL,CALENDAR
+    authenticate_tool("GMAIL", "user_1234567890")
+    authenticate_tool("GCAL", "user_1234567890")
 
-
-    mcp_response = scalekit_client.connect.create_mcp(
-        identifier = "default",
+    mcp_response = connect.create_mcp(
+        identifier = "user_1234567890",
         tool_mappings = [
             ToolMapping(
                 tool_names=["gmail_fetch_mails", "gmail_send_mails"],
@@ -26,16 +55,31 @@ def main():
             ),
             ToolMapping(
                 tool_names=["googlecalendar_list_events", "googlecalendar_create_event"],
-                connection_name="CALENDAR",
+                connection_name="GCAL",
             )
         ]
     )
 
+    from langchain_mcp_adapters.client import MultiServerMCPClient
+    from langgraph.prebuilt import create_react_agent
+
     # Print the URL of the created MCP
     print("MCP created successfully:", mcp_response.url)
 
+    client = MultiServerMCPClient(
+        {
+            "gcal_demo": {
+                "transport": "streamable_http",
+                "url": mcp_response.url
+            },
+        }
+    )
+    tools = await client.get_tools()
+    agent = create_react_agent("openai:gpt-4.1", tools)
+    gcal_response = await agent.ainvoke({"messages": "show me my calendar events for today"})
+
+    print(gcal_response)
 
 
-if __name__ == "__main__":
-    main()
+asyncio.run(main())
 
